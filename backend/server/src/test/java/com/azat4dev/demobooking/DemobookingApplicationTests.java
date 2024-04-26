fbf06683e926c1d7e6de24b699c4f88fc6240710
Azat <azat4dev@gmail.com>
2024-04-26 23:45:49 +0300

    Initialize server application

---
+++ backend/server/src/test/java/com/azat4dev/demobooking/DemobookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azat4dev.demobooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemobookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
