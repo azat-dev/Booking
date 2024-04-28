fe2b363b49d94d66cb142e6f5faa5dff5a527fa2
Azat <azat4dev@gmail.com>
2024-04-28 18:48:23 +0300

    Add verificationTokensService to config

--- backend/server/src/main/java/com/azat4dev/demobooking/users/application/config/DataConfig.java
+++ backend/server/src/main/java/com/azat4dev/demobooking/users/application/config/DataConfig.java
@@ -3,11 +3,14 @@ package com.azat4dev.demobooking.users.application.config;
 import com.azat4dev.demobooking.common.EventsStore;
 import com.azat4dev.demobooking.common.utils.SystemTimeProvider;
 import com.azat4dev.demobooking.common.utils.TimeProvider;
+import com.azat4dev.demobooking.users.data.services.VerificationTokensServiceImpl;
 import com.azat4dev.demobooking.users.domain.interfaces.repositories.UsersRepository;
 import com.azat4dev.demobooking.users.domain.interfaces.services.EmailService;
 import com.azat4dev.demobooking.users.domain.interfaces.services.EncodedPassword;
 import com.azat4dev.demobooking.users.domain.interfaces.services.PasswordService;
+import com.azat4dev.demobooking.users.domain.services.VerificationTokensService;
 import com.azat4dev.demobooking.users.domain.values.Password;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.token.TokenService;
@@ -27,8 +30,18 @@ public class DataConfig {
     }
 
     @Bean
-    TokenService tokenService() {
-        return null;
+    VerificationTokensService verificationTokensService(
+        @Value("app.security.verification_token.secret")
+        String jwtSecret,
+        @Value("app.security.verification_token.lifetimeMs")
+        long verificationTokensLifeTimeMs,
+        TimeProvider timeProvider
+    ) {
+        return new VerificationTokensServiceImpl(
+            jwtSecret,
+            verificationTokensLifeTimeMs,
+            timeProvider
+        );
     }
 
     @Bean
--- backend/server/src/main/java/com/azat4dev/demobooking/users/domain/services/VerificationTokensService.java
+++ backend/server/src/main/java/com/azat4dev/demobooking/users/domain/services/VerificationTokensService.java
@@ -2,8 +2,6 @@ package com.azat4dev.demobooking.users.domain.services;
 
 import com.azat4dev.demobooking.users.domain.values.UserId;
 
-import java.util.Date;
-
 public interface VerificationTokensService {
 
     VerificationToken makeVerificationToken(UserId userId);
