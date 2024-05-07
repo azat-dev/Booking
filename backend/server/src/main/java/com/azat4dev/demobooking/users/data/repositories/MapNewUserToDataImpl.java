45cf81b2969a33d5e5c1f541fb3a456d0d75ec9a
Azat <azat4dev@gmail.com>
2024-05-07 20:30:00 +0300

    Fix MapNewUserToDataImpl

--- backend/server/src/main/java/com/azat4dev/demobooking/users/data/repositories/MapNewUserToDataImpl.java
+++ backend/server/src/main/java/com/azat4dev/demobooking/users/data/repositories/MapNewUserToDataImpl.java
@@ -3,7 +3,7 @@ package com.azat4dev.demobooking.users.data.repositories;
 import com.azat4dev.demobooking.users.data.entities.UserData;
 import com.azat4dev.demobooking.users.domain.interfaces.repositories.NewUserData;
 
-public class MapNewUserToDataImpl implements MapNewUserToData {
+public final class MapNewUserToDataImpl implements MapNewUserToData {
 
     @Override
     public UserData map(NewUserData newUserData) {
@@ -11,9 +11,9 @@ public class MapNewUserToDataImpl implements MapNewUserToData {
             newUserData.userId().value(),
             newUserData.createdAt(),
             newUserData.createdAt(),
+            newUserData.email().toString(),
             newUserData.fullName().firstName().getValue(),
             newUserData.fullName().lastName().getValue(),
-            newUserData.email().toString(),
             newUserData.encodedPassword().value(),
             newUserData.emailVerificationStatus()
         );
