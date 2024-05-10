e4e68b8912ad58d0875bea6c6f701d341f3514d0
Azat <azat4dev@gmail.com>
2024-05-10 12:09:21 +0300

    Refactor the code

--- backend/server/src/main/java/com/azat4dev/demobooking/users/users_queries/domain/interfaces/repositories/UsersReadRepositoryImpl.java
+++ backend/server/src/main/java/com/azat4dev/demobooking/users/users_queries/domain/interfaces/repositories/UsersReadRepositoryImpl.java
@@ -3,20 +3,14 @@ package com.azat4dev.demobooking.users.users_queries.domain.interfaces.repositor
 import com.azat4dev.demobooking.users.common.domain.values.UserId;
 import com.azat4dev.demobooking.users.users_queries.data.dao.UsersReadDao;
 import com.azat4dev.demobooking.users.users_queries.domain.entities.PersonalUserInfo;
+import lombok.RequiredArgsConstructor;
 
 import java.util.Optional;
 
+@RequiredArgsConstructor
 public class UsersReadRepositoryImpl implements UsersReadRepository {
     private final UsersReadDao usersReadDao;
     private final MapUserRecordToPersonalInfo mapUserRecordToPersonalInfo;
-
-    public UsersReadRepositoryImpl(
-        UsersReadDao usersReadDao,
-        MapUserRecordToPersonalInfo mapUserRecordToPersonalInfo
-    ) {
-        this.usersReadDao = usersReadDao;
-        this.mapUserRecordToPersonalInfo = mapUserRecordToPersonalInfo;
-    }
 
     @Override
     public Optional<PersonalUserInfo> getPersonalUserInfoById(UserId id) {
