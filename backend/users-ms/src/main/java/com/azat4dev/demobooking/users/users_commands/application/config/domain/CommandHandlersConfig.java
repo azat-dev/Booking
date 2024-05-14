9724aa404d5b70564149ae188fcf5b6450a6a027
Azat <azat4dev@gmail.com>
2024-05-14 12:39:35 +0300

    Clean the code

--- backend/users-ms/src/main/java/com/azat4dev/demobooking/users/users_commands/application/config/domain/CommandHandlersConfig.java
+++ backend/users-ms/src/main/java/com/azat4dev/demobooking/users/users_commands/application/config/domain/CommandHandlersConfig.java
@@ -16,7 +16,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
