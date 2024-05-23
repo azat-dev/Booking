a51df73d2d01bedea0fbf2cefcecc672b92e8aa6
Azat <azat4dev@gmail.com>
2024-05-23 13:59:38 +0300

    Add listings microservice

---
+++ backend/listings-ms/src/main/java/com/azat4dev/bookingdemo/listingsms/ListingsMsApplication.java
@@ -0,0 +1,13 @@
+package com.azat4dev.bookingdemo.listingsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListingsMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListingsMsApplication.class, args);
+	}
+
+}
---
+++ backend/listings-ms/src/test/java/com/azat4dev/bookingdemo/listingsms/ListingsMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azat4dev.bookingdemo.listingsms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListingsMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
