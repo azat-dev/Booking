f193e87ba4fd37e21fbb74d584bdfaa7f2b6afc4
Azat <azat4dev@gmail.com>
2024-05-31 01:49:39 +0300

    Add additional fields to listing

---
+++ backend/listings-ms/src/main/java/com/azat4dev/booking/listingsms/queries/domain/values/GuestsCapacity.java
@@ -0,0 +1,49 @@
+package com.azat4dev.booking.listingsms.queries.domain.values;
+
+import com.azat4dev.booking.shared.domain.DomainException;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@EqualsAndHashCode
+@Getter
+public final class GuestsCapacity {
+    private final int adults;
+    private final int children;
+    private final int infants;
+
+    // Exceptions
+
+    public static GuestsCapacity checkAndMake(int adults, int children, int infants) throws Exception.CapacityMustBePositive {
+        if (adults <= 0) {
+            throw new Exception.CapacityMustBePositive("adults");
+        }
+        if (children < 0) {
+            throw new Exception.CapacityMustBePositive("children");
+        }
+        if (infants < 0) {
+            throw new Exception.CapacityMustBePositive("infants");
+        }
+        return new GuestsCapacity(adults, children, infants);
+    }
+
+    public static GuestsCapacity dangerouslyMake(int adults, int children, int infants) {
+        return new GuestsCapacity(adults, children, infants);
+    }
+
+    // Exceptions
+
+    public static abstract class Exception extends DomainException {
+        public Exception(String message) {
+            super(message);
+        }
+
+        public static final class CapacityMustBePositive extends Exception {
+            public CapacityMustBePositive(String field) {
+                super("Guests capacity must be positive: " + field);
+            }
+        }
+    }
+}
---
+++ backend/listings-ms/src/main/java/com/azat4dev/booking/listingsms/queries/domain/values/PropertyType.java
@@ -0,0 +1,19 @@
+package com.azat4dev.booking.listingsms.queries.domain.values;
+
+public enum PropertyType {
+
+    APARTMENT,
+    HOUSE,
+    VILLA,
+    CABIN,
+    COTTAGE,
+    CHALET,
+    BUNGALOW,
+    TOWNHOUSE,
+    GUESTHOUSE,
+    LOFT,
+    HOSTEL,
+    BOAT,
+    BEDANDBREAKFAST,
+    OTHER
+}
