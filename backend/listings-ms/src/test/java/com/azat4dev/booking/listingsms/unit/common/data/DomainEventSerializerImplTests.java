1db48b3adb10e8869966a539d4c517d98065ac21
Azat <azat4dev@gmail.com>
2024-06-03 00:34:52 +0300

    Update DomainEventSerializerImplTests

--- backend/listings-ms/src/test/java/com/azat4dev/booking/listingsms/unit/common/data/DomainEventSerializerImplTests.java
+++ backend/listings-ms/src/test/java/com/azat4dev/booking/listingsms/unit/common/data/DomainEventSerializerImplTests.java
@@ -1,7 +1,9 @@
 package com.azat4dev.booking.listingsms.unit.common.data;
 
 import com.azat4dev.booking.listingsms.commands.data.serializer.DomainEventsSerializerImpl;
+import com.azat4dev.booking.listingsms.commands.domain.events.FailedGenerateUrlForUploadListingPhoto;
 import com.azat4dev.booking.listingsms.commands.domain.events.FailedToAddNewListing;
+import com.azat4dev.booking.listingsms.commands.domain.events.GeneratedUrlForUploadListingPhoto;
 import com.azat4dev.booking.listingsms.commands.domain.events.NewListingAdded;
 import com.azat4dev.booking.listingsms.commands.domain.values.ListingTitle;
 import com.azat4dev.booking.shared.data.serializers.DomainEventSerializer;
@@ -9,16 +11,19 @@ import com.azat4dev.booking.shared.domain.values.IdempotentOperationId;
 import com.azat4dev.booking.shared.domain.values.files.BucketName;
 import com.azat4dev.booking.shared.domain.values.files.MediaObjectName;
 import com.azat4dev.booking.shared.domain.values.files.PhotoFileExtension;
+import com.azat4dev.booking.shared.domain.values.files.UploadFileFormData;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
 import java.net.MalformedURLException;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 
 import static com.azat4dev.booking.listingsms.unit.commands.domain.entities.DomainHelpers.anyListingId;
 import static com.azat4dev.booking.listingsms.unit.helpers.EventHelpers.eventsFactory;
 import static com.azat4dev.booking.listingsms.unit.helpers.ListingHelpers.anyOwnerId;
+import static com.azat4dev.booking.listingsms.unit.helpers.ListingHelpers.anyUserId;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
@@ -47,6 +52,29 @@ public class DomainEventSerializerImplTests {
                     anyListingId(),
                     anyOwnerId(),
                     ListingTitle.dangerouslyMakeFrom("title")
+                )
+            ),
+
+            eventsFactory.issue(
+                new GeneratedUrlForUploadListingPhoto(
+                    anyUserId(),
+                    anyListingId(),
+                    new UploadFileFormData(
+                        new java.net.URL("https://example.com"),
+                        BucketName.checkAndMake("bucket"),
+                        MediaObjectName.checkAndMakeFrom("object"),
+                        Map.of("key1", "value1")
+                    )
+                )
+            ),
+
+            eventsFactory.issue(
+                new FailedGenerateUrlForUploadListingPhoto(
+                    IdempotentOperationId.checkAndMakeFrom("operationId"),
+                    anyUserId(),
+                    anyListingId(),
+                    PhotoFileExtension.checkAndMakeFrom("jpg"),
+                    100
                 )
             )
         );
